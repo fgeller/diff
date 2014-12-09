dcaf6cf0908f6183a834a86b95de3461cf2c7337
Felix Geller <fgeller@gmail.com>
2014-12-09 20:14:04 +1300

    Renames and error handling.

--- diff.go
+++ diff.go
@@ -1,33 +1,71 @@
 package main
 
 import "fmt"
+import "errors"
 
-// Let P_ij be the length of the longest subsequence common to the first i lines
-// of the first file a and the first j lines of the second b.
-
-func lcs(i int, a []string, j int, b []string) int {
-	// TODO: err -- bounds check
-	if i == 0 || j == 0 {
-		return 0
+func longestCommonSubsequence(leftLimit int, leftFile []string, rightLimit int, rightFile []string) (int, error) {
+	if leftLimit == 0 || rightLimit == 0 {
+		return 0, nil
+	}
+	if leftLimit > len(leftFile) {
+		message := fmt.Sprintf(
+			"Out of bounds for left file: [%v] too large for slice of length [%v]",
+			leftLimit,
+			len(leftFile)
+		)
+		return 0, errors.New(message)
+	}
+	if rightLimit > len(rightFile) {
+		message := fmt.Sprintf(
+			"Out of bounds for right file: [%v] too large for slice of length [%v]",
+			rightLimit,
+			len(rightFile)
+		)
+		return 0, errors.New(message)
 	}
 
-	if a[i-1] == b[j-1] {
-		return 1 + lcs(i-1, a, j-1, b)
+
+
+	if leftFile[leftLimit-1] == rightFile[rightLimit-1] {
+		lcs, err := longestCommonSubsequence(leftLimit-1, leftFile, rightLimit-1, rightFile)
+		if err != nil {
+			return 0, err
+		}
+
+		return 1+lcs, nil
 	}
 
-	i1j := lcs(i-1, a, j, b)
-	ij1 := lcs(i, a, j-1, b)
-	if i1j > ij1 {
-		return i1j
+	maxLeft, err := longestCommonSubsequence(leftLimit-1, leftFile, rightLimit, rightFile)
+	if err != nil {
+		return 0, err
 	}
-	return ij1
+	maxRight, err := longestCommonSubsequence(leftLimit, leftFile, rightLimit-1, rightFile)
+	if err != nil {
+		return 0, err
+	}
+
+	if maxLeft > maxRight {
+		return maxLeft, nil
+	}
+	return maxRight, nil
 }
 
 func main() {
-	fmt.Printf("%v ?= 0 - lcs(0, {}, 0, {})\n", lcs(0, []string{}, 0, []string{}))
-	fmt.Printf("%v ?= 1 - lcs(1, {'a'}, 1, {'a'})\n", lcs(1, []string{"a"}, 1, []string{"a"}))
-	fmt.Printf("%v ?= 1 - lcs(2, {'a', 'b'}, 1, {'a'})\n", lcs(2, []string{"a", "b"}, 1, []string{"a"}))
-	fmt.Printf("%v ?= 2 - lcs(1, {'a', 'b'}, 2, {'a', 'b'})\n", lcs(2, []string{"a", "b"}, 2, []string{"a", "b"}))
-	fmt.Printf("%v ?= 1 - lcs(1, {'a'}, 2, {'a', 'b'})\n", lcs(1, []string{"a"}, 2, []string{"a", "b"}))
-	fmt.Printf("%v ?= 1 - lcs(1, {'b'}, 2, {'a', 'b'})\n", lcs(1, []string{"b"}, 2, []string{"a", "b"}))
+	lcs, _ := longestCommonSubsequence(0, []string{}, 0, []string{})
+	fmt.Printf("%v ?= 0 - lcs(0, {}, 0, {})\n", lcs)
+
+	lcs, _ = longestCommonSubsequence(1, []string{"a"}, 1, []string{"a"})
+	fmt.Printf("%v ?= 1 - lcs(1, {'a'}, 1, {'a'})\n", lcs)
+
+	lcs, _ = longestCommonSubsequence(2, []string{"a", "b"}, 1, []string{"a"})
+	fmt.Printf("%v ?= 1 - lcs(2, {'a', 'b'}, 1, {'a'})\n", lcs)
+
+	lcs, _ = longestCommonSubsequence(2, []string{"a", "b"}, 2, []string{"a", "b"})
+	fmt.Printf("%v ?= 2 - lcs(1, {'a', 'b'}, 2, {'a', 'b'})\n", lcs)
+
+	lcs, _ = longestCommonSubsequence(1, []string{"a"}, 2, []string{"a", "b"})
+	fmt.Printf("%v ?= 1 - lcs(1, {'a'}, 2, {'a', 'b'})\n", lcs)
+
+	lcs, _ = longestCommonSubsequence(1, []string{"b"}, 2, []string{"a", "b"})
+	fmt.Printf("%v ?= 1 - lcs(1, {'b'}, 2, {'a', 'b'})\n", lcs)
 }
